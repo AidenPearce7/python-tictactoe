c1e2b7897043d78117a987a1f546d15673b3cd6d
AidenPearce7 <30537758+AidenPearce7@users.noreply.github.com>
2021-05-27 12:24:37 +0200

    Reworked ui drawing

--- src/opencv_backend/ui.py
+++ src/opencv_backend/ui.py
@@ -1,45 +1,63 @@
-"""Collection of functions used for UI"""
+"""UI class"""
 import cv2 as cv
+import numpy as np
 
 
-def draw_grid(frame):
-    """Draws a 3 by 3 grid on the frame"""
-    height, width, _ = frame.shape
-    for i in range(1, 3):
-        startpoint_height = (0, i * height // 3)
-        startpoint_width = (i * width // 3, 0)
-        endpoint_height = (width, i * height // 3)
-        endpoint_width = (i * width // 3, height)
-        thickness = 9
-        color = (255, 0, 0)
-        frame = cv.line(frame, startpoint_height, endpoint_height, color, thickness)
-        frame = cv.line(frame, startpoint_width, endpoint_width, color, thickness)
+class UI:
+    """Handles UI drawing and managing"""
 
+    def __init__(self, frame):
+        height, width, channels = frame.shape
+        self.width = width
+        self.height = height
+        self.separators = {
+            "y": (0, height // 3, 2 * height // 3),
+            "x": (0, width // 3, 2 * width // 3),
+        }
+        self.figure = np.zeros((width, height, channels))
+        self.grid_drawn = False
 
-def draw_x(frame, grid_location, color=(0, 0, 255), thickness=7):
-    """Draws X on the selected grid marker.\n
-    location should be a tuple with two numbers indicating place on the grid"""
-    height, width, _ = frame.shape
-    width_offset = width // 3 * 0.25
-    height_offset = height // 3 * 0.25
+    def draw_grid(self, color=(255, 0, 0), thickness=9):
+        """Draws a 3 by 3 grid on the frame"""
+        if not self.grid_drawn:
+            for i in range(1, 3):
+                startpoint_height = (0, self.separators["y"][i])
+                startpoint_width = (self.separators["x"][i], 0)
+                endpoint_height = (self.width, self.separators["y"][i])
+                endpoint_width = (self.separators["x"][i], self.height)
+                self.figure = cv.line(
+                    self.figure, startpoint_height, endpoint_height, color, thickness
+                )
+                self.figure = cv.line(
+                    self.figure, startpoint_width, endpoint_width, color, thickness
+                )
+            self.grid_drawn = True
 
-    left = int(grid_location[0] * width // 3 + width_offset)
-    up = int(grid_location[1] * height // 3 + height_offset)
-    right = int((grid_location[0] + 1) * width // 3 - width_offset)
-    down = int((grid_location[1] + 1) * height // 3 - height_offset)
-    frame = cv.line(frame, (left, up), (right, down), color, thickness)
-    frame = cv.line(frame, (left, down), (right, up), color, thickness)
+    def draw_x(self, grid_location, color=(0, 0, 255), thickness=7):
+        """Draws X on the selected grid marker.\n
+        location should be a tuple with two numbers indicating place on the grid"""
+        width_offset = self.separators["x"][1] * 0.25
+        height_offset = self.separators["y"][1] * 0.25
 
+        left = int(self.separators["x"][grid_location[0]] + width_offset)
+        up = int(self.separators["y"][grid_location[1]] + height_offset)
+        right = int(self.separators["x"][grid_location[0]] + width_offset * 3)
+        down = int(self.separators["y"][grid_location[1]] + height_offset * 3)
+        self.figure = cv.line(self.figure, (left, up), (right, down), color, thickness)
+        self.figure = cv.line(self.figure, (left, down), (right, up), color, thickness)
 
-def draw_circle(frame, grid_location, color=(0, 0, 255), thickness=7):
-    """Draws circle on the selected grid marker.\n
-    location should be a tuple with two numbers indicating place on the grid"""
-    height, width, _ = frame.shape
-    width_offset = width // 3 * 0.5
-    height_offset = height // 3 * 0.5
-    center = (
-        int(grid_location[0] * width // 3 + width_offset),
-        int(grid_location[1] * height // 3 + height_offset),
-    )
-    radius = int(height_offset * 0.75)
-    cv.circle(frame, center, radius, color, thickness)
+    def draw_circle(self, grid_location, color=(0, 0, 255), thickness=7):
+        """Draws circle on the selected grid marker.\n
+        location should be a tuple with two numbers indicating place on the grid"""
+        width_offset = self.separators["x"][1] * 0.5
+        height_offset = self.separators["y"][1] * 0.5
+        center = (
+            int(self.separators["x"][grid_location[0]] + width_offset),
+            int(self.separators["y"][grid_location[1]] + height_offset),
+        )
+        radius = int(height_offset * 0.75)
+        self.figure = cv.circle(self.figure, center, radius, color, thickness)
+
+    def get_separators(self):
+        """Returns the separators used for the processing"""
+        return self.separators
